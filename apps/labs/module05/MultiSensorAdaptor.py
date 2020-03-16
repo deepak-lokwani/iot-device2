c73a1d0e1ebf998c3e3adb5f7795eb66749e5251
deepak-lokwani <60949787+deepak-lokwani@users.noreply.github.com>
2020-03-16 15:33:40 -0400

    module05
    this module contains redis database incorporation for sensor data

---
+++ apps/labs/module05/MultiSensorAdaptor.py
@@ -0,0 +1,42 @@
+'''
+Created on 08-Feb-2020
+
+@author: deepak_lokwani
+'''
+
+'''
+this python script starts the entire Humidity and Temperature Handling process here. This is the main/start process
+'''
+
+from labs.module05 import HumiditySensorAdaptorTask
+from labs.module05 import HI2CSensorAdaptorTask
+from labs.module05 import TempSensorAdaptorTask
+import sys
+from time import sleep
+
+
+
+sys.path.append('/home/pi/workspace/iot-device/apps')
+# print(sys.path)
+
+
+'''
+ instance of temperature and humidity Emulators created with threshold value
+'''
+
+simulator = HumiditySensorAdaptorTask.HumiditySensorAdaptorTask()
+simulator2 = HI2CSensorAdaptorTask.I2CSenseHatAdaptor()
+simulator3 = TempSensorAdaptorTask.TempSensorAdaptorTask(0.1)
+
+# starting my threads
+#sleep(2)
+#simulator.start()
+#sleep(2)
+#simulator2.start()
+#sleep(2)
+simulator3.start()
+
+# enabling the emulators
+simulator.setEmulator(True)
+simulator2.setEmulator(True)
+simulator3.setEmulator(True)
---
+++ apps/labs/module05/SensorDataManager.py
@@ -0,0 +1,63 @@
+'''
+Created on 08-Feb-2020
+
+@author: deepa
+'''
+
+'''
+This class  manages and process all the sensor data that is received as per the user-defined/configured threshold settings
+'''
+
+from labs.common import ActuatorData
+from labs.common import ConfigUtil
+from labs.common.PersistenceUtil import PersistenceUtil
+from labs.module05 import MultiActuatorAdaptor
+from labs.module02 import SMTPClientConnector
+
+class SensorDataManager(object):
+    
+    actuator = ActuatorData.ActuatorData()
+    connector = SMTPClientConnector.SMTPClientConnector
+    sensorType = 'None'
+    
+    def __init__(self):
+        self.config = ConfigUtil.ConfigUtil()
+        self.config.loadConfig()
+        self.actuatorAdaptor = MultiActuatorAdaptor.TempActuatorAdaptor()
+        self.persistenceUtil = PersistenceUtil()
+ 
+    
+    def handleSensorData(self, curVal, sensorType, sensorData):
+        '''
+        this method  handles all the sensor data and updates the actuator settings
+        '''
+        if(sensorType == 'temp'):
+            self.nominalTemp = float(self.config.getProperty(self.config.configConst.CONSTRAINED_DEVICE, self.config.configConst.NOMINAL_TEMP))
+       
+            #checks if my current temperature is greater than nominal temperature
+            if(curVal > self.nominalTemp):
+                # set the Actuator data according to current temperature
+                self.actuator.setCommand(ActuatorData.CMD_ON)
+                self.actuator.setStateData('Decrease')
+                self.actuator.setValue(curVal - self.nominalTemp)
+                self.actuatorAdaptor.updateActuator(self.actuator, 'temp')
+#               self.connector.publishMessage('Excessive Temperature Alert', self.actuator)
+                self.persistenceUtil.writeSensorDataToDbms(sensorData)
+            
+            #checks if my current temperature is lesser than nominal temperature
+            elif(curVal < self.nominalTemp):
+                # set the Actuator data according to current temperature
+                self.actuator.setCommand(ActuatorData.CMD_OFF)
+                self.actuator.setStateData('Increase')
+                self.actuator.setValue(curVal - self.nominalTemp)
+                self.actuatorAdaptor.updateActuator(self.actuator, 'temp')
+#               self.connector.publishMessage('Excessive Temperature Alert', self.actuator)
+                self.persistenceUtil.writeSensorDataToDbms(sensorData)
+        
+        #checks for the sensorData coming from the Sensehat API
+        #elif(sensorType == 'humidSH'):
+         #   self.actuatorAdaptor.updateActuator(curVal, 'humidSH')
+        
+        #checks for the sensorData coming from the I2C bus    
+        #elif(sensorType == 'humidI2C'):
+         #   self.actuatorAdaptor.updateActuator(curVal, 'humidI2C')
